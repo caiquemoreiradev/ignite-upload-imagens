654d25c70d7603d012fdacd223767d27c49fb4f7
Caique Moreira <caiquemoreiradev@gmail.com>
2022-02-22 10:16:15 -0300

    challenge done, but i need help to finish

---
+++ src/components/CardList.tsx
@@ -0,0 +1,49 @@
+import { SimpleGrid, useDisclosure } from '@chakra-ui/react';
+import { useState } from 'react';
+import { Card } from './Card';
+import { ModalViewImage } from './Modal/ViewImage';
+
+interface Card {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
+
+interface CardsProps {
+  cards: Card[];
+}
+
+export function CardList({ cards }: CardsProps): JSX.Element {
+  // TODO MODAL USEDISCLOSURE
+
+  // TODO SELECTED IMAGE URL STATE
+
+  // TODO FUNCTION HANDLE VIEW IMAGE
+
+  const { onOpen, isOpen, onClose } = useDisclosure();
+
+  const [currentImageUrl, setCurrentImageUrl] = useState('');
+
+  function handleViewImage(url: string): void {
+    onOpen();
+    setCurrentImageUrl(url);
+  }
+
+  return (
+    <>
+      <SimpleGrid columns={[1, 2, 3]} spacing="40px">
+        {cards.map(card => (
+          <Card key={card.id} data={card} viewImage={handleViewImage} />
+        ))}
+      </SimpleGrid>
+
+      <ModalViewImage
+        isOpen={isOpen}
+        imgUrl={currentImageUrl}
+        onClose={onClose}
+      />
+    </>
+  );
+}
